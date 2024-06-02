cc2a2cd537f42e659b08346a203d374bf8c4e058
msahrana <msahrana@gmail.com>
2024-06-03 01:34:51 +0600

    user collection is created

--- index.js
+++ index.js
@@ -30,8 +30,79 @@ async function run() {
   try {
     const usersCollection = client.db("lifeLineDB").collection("users");
 
+    /* Verify Token Middleware */
+    const verifyToken = (req, res, next) => {
+      const authHeader = req?.headers?.authorization;
+      if (!authHeader) {
+        return res.status(401).send("Unauthorized access");
+      }
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(403).send({message: "forbidden access"});
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({message: "forbidden access"});
+      }
+      next();
+    };
+
+    /* jwt api */
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res.send({token});
+    });
+
+    // app.post("/jwt", async (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "365d",
+    //   });
+    //   res
+    //     .cookie("token", token, {
+    //       httpOnly: true,
+    //       secure: process.env.NODE_ENV === "production",
+    //       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    //     })
+    //     .send({success: true});
+    // });
+
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({success: true});
+        console.log("Logout successful");
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    /* users api */
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = {email: user?.email};
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send({message: "user already exists", insertedId: null});
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
@@ -41,6 +112,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({email});
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
